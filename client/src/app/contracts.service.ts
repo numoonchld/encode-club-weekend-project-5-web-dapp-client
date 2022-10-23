2ceb0ef361dbe6063155942675bd7cf0ac65f509
numoonchld <raghavendra.saralaya@gmail.com>
2022-10-23 22:22:39 +0530

    contract, signer and owner detection functions

--- client/src/app/contracts.service.ts
+++ client/src/app/contracts.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core'
 import { ethers } from 'ethers'
+import * as ContractAddressesJSON from '../assets/contracts/contracts.json'
+import * as LotteryContractJSON from '../assets/contracts/lottery-contract/Lottery.json'
+import * as LotteryTokenContractJSON from '../assets/contracts/lottery-token-contract/LotteryToken.json'
 
 @Injectable({
   providedIn: 'root',
@@ -7,17 +10,33 @@ import { ethers } from 'ethers'
 export class ContractsService {
   currentAccount: string
   isLoggedIn: Boolean
-  // tokenContractAddress: string
-  // tokenContractJSON: any
-  // ballotContractJSON: any
+
+  contractOwner: string
+
+  lotteryContractAddress: string
+  lotteryTokenContractAddress: string
+
+  contractAddressesJSON: {
+    lotteryContractAddress: string
+    lotteryTokenContractAddress: string
+  }
+  lotteryContractJSON: any
+  lotteryTokenContractJSON: any
+
   provider: ethers.providers.BaseProvider
 
   constructor() {
     this.currentAccount = ''
     this.isLoggedIn = false
-    // this.tokenContractAddress = ''
-    // this.tokenContractJSON = G11TokenJSON
-    // this.ballotContractJSON = TokenizedBallotJSON
+    this.contractOwner = ''
+
+    this.contractAddressesJSON = ContractAddressesJSON
+    this.lotteryContractAddress = this.contractAddressesJSON.lotteryContractAddress
+    this.lotteryTokenContractAddress = this.contractAddressesJSON.lotteryTokenContractAddress
+
+    this.lotteryContractJSON = LotteryContractJSON
+    this.lotteryTokenContractJSON = LotteryTokenContractJSON
+
     // this.provider = ethers.getDefaultProvider('goerli')
     this.provider = ethers.getDefaultProvider('http://localhost:8545')
   }
@@ -29,7 +48,8 @@ export class ContractsService {
         console.log('Install MetaMask!')
         window.alert('MetaMask needed to use this site!')
       } else {
-        console.log('Ethereum object found!', ethereum)
+        // console.log('Ethereum object found!', ethereum)
+        console.log('Ethereum object found!')
       }
 
       const accounts = await ethereum.request({ method: 'eth_accounts' })
@@ -37,6 +57,7 @@ export class ContractsService {
       if (accounts.length !== 0) {
         const account = accounts[0]
         console.log('Found authorized account!', account)
+        // console.log('Found authorized account!')
         this.currentAccount = account
 
         this.isLoggedIn = true
@@ -63,5 +84,50 @@ export class ContractsService {
     this.currentAccount = account
     this.isLoggedIn = true
     return this.currentAccount
+  }
+
+  // get metamask wallet signer
+  async getMetamaskWalletSigner(ethereum: any) {
+    const metamaskWalletProvider = new ethers.providers.Web3Provider(ethereum)
+    return metamaskWalletProvider.getSigner()
+  }
+
+  async getLotteryContract(ethereum: any, signer?: ethers.Signer) {
+    let lotteryContract: ethers.Contract
+
+    if (signer) {
+      lotteryContract = new ethers.Contract(
+        this.lotteryContractAddress,
+        this.lotteryContractJSON.abi,
+        signer,
+      )
+    } else {
+      lotteryContract = new ethers.Contract(
+        this.lotteryContractAddress,
+        this.lotteryContractJSON.abi,
+        this.provider,
+      )
+    }
+
+    return lotteryContract
+  }
+
+  // load contract owner
+  async loadContractOwner(ethereum: any) {
+    // const signer = await this.getMetamaskWalletSigner(ethereum)
+    const lotteryContract = await this.getLotteryContract(ethereum)
+    const owner = await lotteryContract['owner']()
+    console.log('owner: ', await owner)
+    this.contractOwner = await owner
+  }
+
+  // is a lottery active
+  determineIsCurrentAccountLotteryContractOwner(): Boolean {
+    console.log('owner: --', this.contractOwner)
+    console.log('loggedIn: ', this.currentAccount)
+    return (
+      this.contractOwner.toLowerCase().trim() ===
+      this.currentAccount.toLowerCase().trim()
+    )
   }
 }
